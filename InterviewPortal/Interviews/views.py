b2ced8033d0d9fe638f903e3b6016cd06cac75f0
jaskaranbhatia <jaskaransingh.singh77@gmail.com>
2021-06-11 02:20:16 +0530

    Added Admin Login/Logout

--- InterviewPortal/Interviews/views.py
+++ InterviewPortal/Interviews/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def home(request):
     return render(request, 'interviews/home.html')
+
+def loginuser(request):
+    if request.method == 'GET':
+        return render(request, 'interviews/loginuser.html', {'form':AuthenticationForm()})
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'interviews/loginuser.html', {'form':AuthenticationForm(), 'error':'Username and password did not match'})
+        else:
+            login(request, user)
+            return redirect('home')
+
+@login_required
+def logoutuser(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('home')
